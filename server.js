91fd02b2167e21d98cd0a0d14acdd8b69af01f20
naszaklasacode <adamek60a@gmail.com>
2025-10-11 15:46:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,28 +6,36 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// Serwujemy pliki statyczne z głównego folderu (bo masz wszystkie pliki w jednym)
 app.use(express.static(__dirname)); 
-
 
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  socket.on("joinRoom", (room) => {
+  // Obsługa dołączenia do pokoju
+  socket.on("joinRoom", ({ room, username }) => {
     socket.join(room);
     socket.room = room;
-    socket.emit("message", `You joined room: ${room}`);
-    socket.to(room).emit("message", `A new user has joined the room`);
+    socket.username = username;
+
+    // Powitanie dla użytkownika, który dołączył
+    socket.emit("message", `Witaj ${username}, dołączyłeś do pokoju: ${room}`);
+
+    // Powiadomienie dla innych użytkowników w pokoju
+    socket.to(room).emit("message", `${username} dołączył do pokoju`);
   });
 
+  // Obsługa wiadomości
   socket.on("chatMessage", (msg) => {
-    if (socket.room) {
-      io.to(socket.room).emit("message", msg);
+    if (socket.room && socket.username) {
+      io.to(socket.room).emit("message", `${socket.username}: ${msg}`);
     }
   });
 
+  // Obsługa rozłączenia
   socket.on("disconnect", () => {
-    if (socket.room) {
-      socket.to(socket.room).emit("message", "A user left the room");
+    if (socket.room && socket.username) {
+      socket.to(socket.room).emit("message", `${socket.username} opuścił pokój`);
     }
     console.log("User disconnected:", socket.id);
   });
@@ -35,4 +43,3 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
